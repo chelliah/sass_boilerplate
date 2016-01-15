f37a3a20597cde087de099daff6c676ef9186fc0
Aron <athomas5@macalester.edu>
2016-01-14 21:57:22 -0600

    add pages for projects

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+router.get('/fuschia', function(req, res){
+    //var file = req.params[0] || "/assets/views/welcome.html";
+    res.sendFile(path.join(__dirname, "../public/", "/assets/views/fuschia.html"));
+});
+router.get('/open_arms', function(req, res){
+    //var file = req.params[0] || "/assets/views/welcome.html";
+    res.sendFile(path.join(__dirname, "../public/", "/assets/views/open_arms.html"));
+});
+
 router.get('/main', function(req, res){
     //var file = req.params[0] || "/assets/views/welcome.html";
     res.sendFile(path.join(__dirname, "../public/", "/assets/views/index.html"));
