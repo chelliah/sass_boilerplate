6d9d22ec4e5e3e653bf1499471e6b92f803c9c46
Aron <athomas5@macalester.edu>
2016-01-29 03:36:28 -0600

    mobile redirect to main page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var mobile = require('mobile-detect');
 
 //router.get('/fuschia', function(req, res){
 //    //var file = req.params[0] || "/assets/views/welcome.html";
@@ -23,12 +24,24 @@ router.get('/contact', function(req, res){
 
 router.get('/main', function(req, res){
     //var file = req.params[0] || "/assets/views/welcome.html";
+
     res.sendFile(path.join(__dirname, "../public/", "/assets/views/index.html"));
 });
 
+router.get('/', function(req, res){
+    //var file = req.params[0] || "/assets/views/welcome.html";
+    var md = new mobile(req.headers['user-agent']);
+    if(md.mobile()){
+        res.redirect('/main')
+    }else{
+        res.sendFile(path.join(__dirname, "../public/", "/assets/views/welcome.html"));
+    }
+});
+
+
 router.get('/*', function(req, res){
     //var file = req.params[0] || "/assets/views/welcome.html";
-    res.sendFile(path.join(__dirname, "../public/", "/assets/views/welcome.html"));
+    res.redirect('/main')
 });
 
 module.exports = router;
